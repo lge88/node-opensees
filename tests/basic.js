c9bf396423d102b8c351585f1c2f2e409af802f4
Li Ge <lge@ucsd.edu>
2013-02-28 20:17:55 -0800

    get basic.js back to work

--- tests/basic.js
+++ tests/basic.js
@@ -9,19 +9,12 @@ ops.on('stderr', function(str) {
   console.log('stderr:' + str);
 });
 
-console.log('initialized!');
-ops.interp('puts "current dir: [pwd]"');
-ops.interp('puts "RES: $RES"');
-ops.interp('puts "INIT_PATH: $INIT_PATH"');
-ops.interp('puts "TMP: $RES"');
-ops.interp('disable_recorder');
-ops.interp('disable_analyze');
-ops.interp('enable_model_history');
-
-ops.interp('cd $RES/projects/truss');
-ops.interp('puts "current dir: [pwd]"');
-ops.interp('source Truss.tcl');
-ops.interp('enable_analyze');
-ops.interp('analyze 1 asdf');
-ops.interp('puts [nodeDisp 4]');
-// ops.interp('print_cmd_history');
+ops.on('ready', function() {
+  console.log('initialized!');
+  ops.interp('puts "current dir: [pwd]"');
+  ops.interp('puts "RES: $RES_PATH"');
+  ops.interp('puts "INIT_PATH: $INIT_PATH"');
+  ops.interp('puts "TMP: $TMP_PATH"');
+  ops.interp('puts "__DATA_STREAM_FILE: $__DATA_STREAM_FILE"');
+  ops.interp('puts "__DATA_STREAM_FD: $__DATA_STREAM_FD"');
+});
