e464e54258e28f1c80f79264b861ef8000ab58d3
Li Ge <lge@ucsd.edu>
2013-03-06 12:13:26 -0800

    try to get recorder work. but the data writes to recorder can not be flushed since no descriptor from opensees available after adding it.

---
+++ tests/recorder.js
@@ -0,0 +1,46 @@
+var OpenSees= require('../index.js');
+var ops = new OpenSees();
+
+ops.on('stdout', function(str) {
+  console.log('stdout:' + str);
+});
+
+ops.on('stderr', function(str) {
+  console.log('stderr:' + str);
+});
+
+ops.on('data', function(d) {
+  console.log('data:' + d);
+});
+
+ops.on('ready', function(d) {
+  ops.interp('disable_recorder');
+  ops.interp('disable_analyze');
+  
+  ops.interp('cd $RES_PATH/projects/truss');
+  ops.interp('puts "current dir: [pwd]"');
+  ops.interp('source Truss.tcl');
+  
+  ops.interp('enable_recorder');
+  ops.interp('recorder Node -file $__DATA_STREAM_FILE -node 1 2 3 4 -dof 1 2 disp');
+  
+  ops.interp('enable_analyze');
+  ops.interp('analyze 1');
+  // ops.interp('record');
+  
+  // ops.interp('puts $__DATA_STREAM_FD "node 4: [nodeDisp 4]"');
+
+  // The recorder doen't work right now. Because the data in the channel doen't get flushed.
+  // Opensees provide no way to get fd of a recorder
+  // flushData try to flush different channel
+  // ops.flushData();
+
+  // Try to hack here, guess the file descriptor is file4, doesn't work
+  // ops.interp('flush file4');
+  
+  // Call exit can flush all data to file
+  ops.interp('exit');
+  
+  // console.log('pid:', ops.interpreter.pid);
+  // ops.interp('flush $__DATA_STREAM_FD');
+});
